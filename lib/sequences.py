7a0603bf823e7afcd6b551300f9c496242c6193a
lionard <leonard.muhati@student.moringaschool.com>
2024-09-26 10:33:18 +0300

    compelete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length==0:
+        print ([])
+        return
+    elif length==1:
+        print([0])
+        return
+    elif length==2:
+        print([0, 1])
+        return
+    elif length==10:
+        fib_sequence = [0, 1]
+        for i in range(2, 10):
+            fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
+        print(fib_sequence)
+        
